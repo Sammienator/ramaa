b6bb376dbd75b417faf5b6085462a0c44427458e
Sammienator <sammugo18@gmail.com>
2023-09-29 11:06:17 +0300

    changes and additions to components

--- src/components/About.js
+++ src/components/About.js
@@ -29,7 +29,7 @@ const About = () => {
             <path d="M23 21v-2a4 4 0 00-3-3.87m-4-12a4 4 0 010 7.75"></path>
           </svg>
           <h2 class="title-font font-medium text-3xl text-gray-900">1.3K</h2>
-          <p class="leading-relaxed">Users</p>
+          <p class="leading-relaxed">Songs Written</p>
         </div>
       </div>
       <div class="p-4 md:w-1/4 sm:w-1/2 w-full">
@@ -39,7 +39,7 @@ const About = () => {
             <path d="M21 19a2 2 0 01-2 2h-1a2 2 0 01-2-2v-3a2 2 0 012-2h3zM3 19a2 2 0 002 2h1a2 2 0 002-2v-3a2 2 0 00-2-2H3z"></path>
           </svg>
           <h2 class="title-font font-medium text-3xl text-gray-900">74</h2>
-          <p class="leading-relaxed">Files</p>
+          <p class="leading-relaxed">Music Videos</p>
         </div>
       </div>
       <div class="p-4 md:w-1/4 sm:w-1/2 w-full">
@@ -48,7 +48,7 @@ const About = () => {
             <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path>
           </svg>
           <h2 class="title-font font-medium text-3xl text-gray-900">46</h2>
-          <p class="leading-relaxed">Places</p>
+          <p class="leading-relaxed">Social Media PLatforms</p>
         </div>
       </div>
     </div>
